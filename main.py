891cd75bd7aa87ad25be7a469a9881a4421006f6
Ahmed Hamdi <ahmed.hamdi.inf@gmail.com>
2026-05-05 21:41:06 +0100

    readme file

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import subprocess
+import sys
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def run_streamlit():
+    """Launch the Streamlit dashboard."""
+    subprocess.run(
+        [sys.executable, "-m", "streamlit", "run", "app.py"],
+        check=True,
+    )
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    run_streamlit()
